891cdca9f0338f2fb0e4a7cd03530a2e52b32a17
Bryan Lai <bryanlais@gmail.com>
2023-12-06 15:33:14 +0800

    Improve script & logging

--- scripts/wingcool-bind.sh
+++ scripts/wingcool-bind.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # set up the wingcool touchscreen
 
-set -x
+# print a timestamp
+echo
+date --rfc-3339=s
 
+# sleep a bit
+sleep 0.5
 export DISPLAY=:0
 export XAUTHORITY=/run/user/1000/gdm/Xauthority
 
+set -x
 xinput-json \
   | jq '.[] | select( .name == "WingCool Inc. TouchScreen" ) | .id' \
   | xargs -I _ xinput map-to-output _ DP-1
